c550c780b3278b6f7f192264afb2287878089071
Sergey <sernort1990@gmail.com>
2024-02-08 14:39:49 +0300

    cors

--- server/config/serverConfig.js
+++ server/config/serverConfig.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const verifyAccessToken = require("../middleware/verifyJWT");
 
 const serverConfig = (app) => {
   app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    }),
+    // cors({
+    //   origin: "http://localhost:5173",
+    //   credentials: true,
+    // }),
   );
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
