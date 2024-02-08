e74116c6ea6486c340d4f128c2b2d06eb4e163c9
Sergey <sernort1990@gmail.com>
2024-02-08 22:08:25 +0300

    serverConfig

--- server/config/serverConfig.js
+++ server/config/serverConfig.js
@@ -5,11 +5,19 @@ const cookieParser = require("cookie-parser");
 const verifyAccessToken = require("../middleware/verifyJWT");
 
 const serverConfig = (app) => {
+  // Добавьте обработчик для запросов метода OPTIONS
+  app.options('*', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://pinter.fun');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(200).send();
+  });
+
   app.use(
     cors({
       origin: "https://pinter.fun",
       credentials: true,
-    }),
+    })
   );
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
