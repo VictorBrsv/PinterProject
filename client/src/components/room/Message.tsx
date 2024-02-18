06136b789765374957200125a462e29f8054bdcb
Natalya Sheremetyeva <ya.sher.n@yandex.ru>
2024-02-19 00:05:36 +0300

    chat scss

--- client/src/components/room/Message.tsx
+++ client/src/components/room/Message.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import styles from './styles/Message.module.scss';
 import userLogo from './styles/user_message_logo.png';
+import { useAppSelector } from '../../redux/store';
 
-export default function Message(): JSX.Element {
+export default function Message({ message }: { message: string }): JSX.Element {
+    const { user } = useAppSelector(state => state.auth);
+    const userAvatar = user?.image || userLogo;
 
     return (
         <div className={styles.message__container}>
-            <div><img src={userLogo} alt="" /></div>
+            <div><img src={userAvatar} alt="" /></div>
             <div className={styles.message}>
-                <h4>Bobr Curva</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis numquam autem ab ipsum quisquam rerum dignissimos alias nihil nostrum, nesciunt perspiciatis quis ipsa molestias consectetur suscipit possimus eligendi, mollitia laboriosam.</p>
+                {/* <h4>Bobr Curva</h4> */}
+                <h4>{user?.name}</h4>
+                <p>{message}</p>
             </div>
         </div>
     )
