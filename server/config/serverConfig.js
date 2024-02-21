7f52dab776d2d45aa5fdf00a7569d99642cdf881
Natalya Sheremetyeva <ya.sher.n@yandex.ru>
2024-02-21 14:12:11 +0300

    build

--- server/config/serverConfig.js
+++ server/config/serverConfig.js
@@ -6,22 +6,22 @@ const verifyAccessToken = require("../middleware/verifyJWT");
 
 const serverConfig = (app) => {
   // Добавьте обработчик для запросов метода OPTIONS
-  // app.options("*", (req, res) => {
-  //   res.setHeader("Access-Control-Allow-Origin", "https://pinter.fun");
-  //   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // добавила методы PUT, DELETE
-  //   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  //   res.status(200).send();
-  // });
+  app.options("*", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://pinter.fun");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // добавила методы PUT, DELETE
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.status(200).send();
+  });
   app.use(express.json({ limit: "200mb" }));
   app.use(express.urlencoded({ limit: "200mb", extended: true }));
   app.use(express.text({ limit: "200mb" }));
   app.use(
     cors({
-      origin: "http://localhost:5173",
-      // origin: "https://pinter.fun",
+      // origin: "http://localhost:5173",
+      origin: "https://pinter.fun",
       credentials: true,
-      // methods: "GET, POST, PUT, DELETE, OPTIONS", // добавила методы PUT, DELETE
-      // allowedHeaders: "Content-Type",
+      methods: "GET, POST, PUT, DELETE, OPTIONS", // добавила методы PUT, DELETE
+      allowedHeaders: "Content-Type",
     }),
   );
   app.use(express.static(path.join(__dirname, "..", "public")));
